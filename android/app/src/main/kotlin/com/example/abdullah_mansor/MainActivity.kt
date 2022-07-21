63e4fff4d61bf15202c671b6a6534cd2a84b5222
Abdalrhman <lorgroup2002@gmail.com>
2022-07-21 19:14:49 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/abdullah_mansor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.abdullah_mansor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:abdullah_mansor/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        home: LoginScreen());
+  }
+}
+
+
